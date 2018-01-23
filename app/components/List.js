2b3204a4a292eba768a0f1d46a987140e91fc83e
Heather Thacker <hhthacker@gmail.com>
2018-01-23 15:47:04 -0600

    attempt comp 2

---
+++ app/components/List.js
@@ -0,0 +1,8 @@
+var ReactDOM = require('react-dom');
+var React = require('react');
+
+function List () {
+        return <p1>worldly lists</p1>;
+}
+
+ReactDOM.render(<List />, document.getElementById('lists'))
