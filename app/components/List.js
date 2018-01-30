6536821d4dab807e92056d0161a34a39f135001b
Heather Thacker <hhthacker@gmail.com>
2018-01-30 09:08:48 -0600

    comp notes

--- app/components/List.js
+++ app/components/List.js
@@ -2,7 +2,7 @@ var ReactDOM = require('react-dom');
 var React = require('react');
 
 function List () {
-        return <p1>worldly lists</p1>;
+        return <p1>Maybe this is the social network?</p1>;
 }
 
 ReactDOM.render(<List />, document.getElementById('lists'))
---
+++ app/components/Soil.js
@@ -0,0 +1,8 @@
+var ReactDOM = require('react-dom');
+var React = require('react');
+
+function Soil () {
+        return <p1>Hello Dirty World. How's your soil health? Let's look.</p1>;
+}
+
+ReactDOM.render(<Soil />, document.getElementById('soil'))
